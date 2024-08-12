9b2bb7bbf5ba8520be86ef42212f3b1e5b3683da
Deepank kartikey <deepankkartikey@gmail.com>
2024-08-11 21:25:49 -0400

    refactored code to methods

--- Apartment-Hunting/find-apartments.py
+++ Apartment-Hunting/find-apartments.py
@@ -1,50 +1,52 @@
 import requests
 from bs4 import BeautifulSoup
+from apscheduler.schedulers.blocking import BlockingScheduler
 
-base_url = "https://www.sleepwellmanagement.com"
-url = "https://www.sleepwellmanagement.com/apartment-search"
-budget = float(input("Enter your rent budget: "))
 
-# Proxy configuration
-# proxy_host = ''
-# proxy_port = 823
-# proxy_login = ''
-# proxy_password = ''
-# proxy = f'http://{proxy_login}:{proxy_password}@{proxy_host}:{proxy_port}'
+def find_apartments(budget):
+    base_url = "https://www.sleepwellmanagement.com"
+    url = "https://www.sleepwellmanagement.com/apartment-search"
 
-proxies = {
-    # 'http': proxy,
-    # 'https': proxy
-}
+    # Proxy configuration
+    proxy_host = ''
+    proxy_port = 823
+    proxy_login = ''
+    proxy_password = ''
+    proxy = f'http://{proxy_login}:{proxy_password}@{proxy_host}:{proxy_port}'
 
-response = requests.get(url=url, proxies=proxies)
-soup = BeautifulSoup(response.text, 'html.parser')
-# print(soup)
+    proxies = {
+        'http': proxy,
+        'https': proxy
+    }
 
-# useful classes in parsed html
-# per listing - nhw-list__item
-# name - nhw-list__location
-# price - nhw-list__price
+    response = requests.get(url=url)
+    soup = BeautifulSoup(response.text, 'html.parser')
 
-apartment_list = soup.find_all(class_="nhw-list__item")
-print(f'Total Apartments found: {len(apartment_list)}')
+    apartment_list = soup.find_all(class_="nhw-list__item")
+    filtered_apartments = []
 
-filtered_apartments = []
+    for apartment in apartment_list:
+        listing_url = apartment.find('a').get('href')
+        listing_url = base_url + listing_url
+        location = apartment.find('div', class_="nhw-list__location").text.strip()
+        price = apartment.find('div', class_="nhw-list__price").text.strip()
+        # convert price to floating point value
+        price = float(price.replace("$", "").replace(",", "").replace("/mo.", ""))
 
-for apartment in apartment_list:
-    # apartment.get()
-    listing_url = apartment.find('a').get('href')
-    listing_url = base_url + listing_url
-    location = apartment.find('div', class_="nhw-list__location").text.strip()
-    price = apartment.find('div', class_="nhw-list__price").text.strip()
-    # convert price to floating point value
-    price = float(price.replace("$", "").replace(",", "").replace("/mo.", ""))
+        if price < budget:
+            filtered_apartments.append({"listing_location": location, "rent": price, "listing_url": listing_url})
 
-    # print(listing_url)
-    # print(location)
-    # print(price)
-    if price < budget:
-        filtered_apartments.append({"listing_location": location, "rent": price, "listing_url": listing_url})
+    return filtered_apartments
 
-print("Aparments with rent in your budget: ", len(filtered_apartments))
-print(filtered_apartments)
+
+def apartment_finder_job():
+    budget = float(input("Enter you budget: "))
+    filtered_apartments = find_apartments(budget)
+    if filtered_apartments:
+        print(f"{ len(filtered_apartments) } listings found.")
+    else:
+        print("No apartments found in your budget.")
+
+
+if __name__ == "__main__":
+    apartment_finder_job()
