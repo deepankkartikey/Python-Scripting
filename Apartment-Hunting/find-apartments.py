6ba65da95c884c0eefd516f18eed935ac1d5fa60
Deepank kartikey <deepankkartikey@gmail.com>
2024-08-12 22:41:25 -0400

    Added Email sending functionality on each run of script

--- Apartment-Hunting/find-apartments.py
+++ Apartment-Hunting/find-apartments.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
-from apscheduler.schedulers.blocking import BlockingScheduler
+import smtplib
+import getpass
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 def find_apartments(budget):
@@ -39,11 +42,37 @@ def find_apartments(budget):
     return filtered_apartments
 
 
+def send_email(apartments, email):
+    print(f"Sending Email to: {email}")
+    sender_email = "listings.sleepwell@gmail.com"
+    sender_password = "qdvp jhsd atrs xbqs"
+
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = email
+    msg['Subject'] = "Filtered Apartments List from Sleepwell Property Management"
+
+    body = "Here are the apartments in your budget: \n"
+    for location, price, url in apartments:
+        body += f"Title: {location}\nRent: {price}\nListing URL: {url}\n\n"
+    
+    msg.attach(MIMEText(body, 'plain'))
+
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.starttls()
+    server.login(sender_email, sender_password)
+    text = msg.as_string()
+    server.sendmail(sender_email, email, text)
+    server.quit()
+
+
 def apartment_finder_job():
     budget = float(input("Enter you budget: "))
+    email = input("Enter email address to receive listings: ")
     filtered_apartments = find_apartments(budget)
     if filtered_apartments:
         print(f"{ len(filtered_apartments) } listings found.")
+        send_email(filtered_apartments, email)
     else:
         print("No apartments found in your budget.")
 
